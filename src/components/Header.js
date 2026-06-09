972c495c0f1f8df49af9683fa84fd149e250a759
ngwenyathembani668-lab <ngwenyathemabni668@gmail.com>
2026-06-09 22:32:40 +0200

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,45 +2,54 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => {
-  return (
-    <>
+    return (
+        <>
 
-    <nav>
+            <nav>
 
-        <div className='logo'>
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKuvYP6vm8lO1UU821piFpPNYQv4v2nJa903fZjk7jEw&s=10' alt='Airbnb logo' />
-        </div>
+                <div className='logo'>
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKuvYP6vm8lO1UU821piFpPNYQv4v2nJa903fZjk7jEw&s=10' alt='Airbnb logo' />
+                </div>
 
-        <div className='navigation'>
+                <div className='navigation'>
 
-            <div className='nav-item'>
-                <span>Places to stay</span>
-                <span className='underline'></span>
-            </div>
-            <div className='nav-item'>
-                <span>Experiences</span>
-                <span className='underline'></span>
-            </div>
-            <div className='nav-item'>
-                <span>Online Experiences</span>
-                <span className='underline'></span>
-            </div>
+                    <div className='nav-item'>
+                        <span>Places to stay</span>
+                        <div className='underline'></div>
+                    </div>
+                    <div className='nav-item'>
+                        <span>Experiences</span>
+                        <div className='underline'></div>
+                    </div>
+                    <div className='nav-item'>
+                        <span>Online Experiences</span>
+                        <div className='underline'></div>
+                    </div>
 
-        </div>
+                </div>
 
-        <div className='user-options'>
+                <div className='user-options'>
 
-            <div clasName='action'>
-                <span>Become a Host</span>
-                
-            </div>
+                    <div className='action'>
+                        <span color='#fff'>Become a Host</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff"><path d="M325-111.5q-73-31.5-127.5-86t-86-127.5Q80-398 80-480.5t31.5-155q31.5-72.5 86-127t127.5-86Q398-880 480.5-880t155 31.5q72.5 31.5 127 86t86 127Q880-563 880-480.5T848.5-325q-31.5 73-86 127.5t-127 86Q563-80 480.5-80T325-111.5ZM480-162q26-36 45-75t31-83H404q12 44 31 83t45 75Zm-104-16q-18-33-31.5-68.5T322-320H204q29 50 72.5 87t99.5 55Zm208 0q56-18 99.5-55t72.5-87H638q-9 38-22.5 73.5T584-178ZM170-400h136q-3-20-4.5-39.5T300-480q0-21 1.5-40.5T306-560H170q-5 20-7.5 39.5T160-480q0 21 2.5 40.5T170-400Zm216 0h188q3-20 4.5-39.5T580-480q0-21-1.5-40.5T574-560H386q-3 20-4.5 39.5T380-480q0 21 1.5 40.5T386-400Zm268 0h136q5-20 7.5-39.5T800-480q0-21-2.5-40.5T790-560H654q3 20 4.5 39.5T660-480q0 21-1.5 40.5T654-400Zm-16-240h118q-29-50-72.5-87T584-782q18 33 31.5 68.5T638-640Zm-234 0h152q-12-44-31-83t-45-75q-26 36-45 75t-31 83Zm-200 0h118q9-38 22.5-73.5T376-782q-56 18-99.5 55T204-640Z" /></svg>
+                    </div>
 
-        </div>
+                    <div className='profile'>
+                        <div className='menu'>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#888"><path d="M120-120v-80h720v80H120Zm0-320v-80h720v80H120Zm0-320v-80h720v80H120Z" /></svg>
+                        </div>
+                        <div className='avatar'>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm146.5-204.5Q340-521 340-580t40.5-99.5Q421-720 480-720t99.5 40.5Q620-639 620-580t-40.5 99.5Q539-440 480-440t-99.5-40.5ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm100-95.5q47-15.5 86-44.5-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160q53 0 100-15.5ZM523-537q17-17 17-43t-17-43q-17-17-43-17t-43 17q-17 17-17 43t17 43q17 17 43 17t43-17Zm-43-43Zm0 360Z" /></svg>
+                        </div>
+                    </div>
 
-    </nav>
-      
-    </>
-  );
+                </div>
+
+            </nav>
+
+        </>
+    );
 };
 
 export default Header;
